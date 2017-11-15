e5900e7e25c79816c550d5e87a3d91089630e37c
Zac-L <Zachlichens@gmail.com>
2017-11-15 10:32:25 -0800

    hopefully created a working Thumbnail component

---
+++ src/components/Thumbnail.js
@@ -0,0 +1,28 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Thumbnail extends PureComponent {
+    
+    static propTypes = {
+      bunnies: PropTypes.arrayOf(PropTypes.object).isRequired
+    }
+
+    constructor() {
+      super();
+    }
+
+    render() {
+      return (
+        <div className="thumbnail">
+          {this.props.bunnies.map((bunny, index) => {
+            return (
+              <div key={index}>
+                <img src={bunny.url} alt={bunny.title}/>
+                <h4>{bunny.title}</h4>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+}
