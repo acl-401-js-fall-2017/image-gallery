03c951ab187cc69debbe098713c8f130f3546f4a
Zac-L <Zachlichens@gmail.com>
2017-11-15 11:21:56 -0800

    test still failing, enzyme error

--- src/__tests__/List.test.js
+++ src/__tests__/List.test.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
+import toJSON from 'enzyme-to-json';
 
 import Bunnies from '../images/bunnies';
 import List from '../components/List';
 
 describe('List', () => {
   it('lists of bunnies', () => {
-    const wrapper = shallow(<list bunnies={Bunnies}/>);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    const wrapper = shallow(<List bunnies={Bunnies}/>);
+    expect(toJSON(wrapper)).toMatchSnapshot();
   });
 });
