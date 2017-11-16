f523660c4ad942c24101f1c6e0cf55b8cc9c8fa8
Zac-L <Zachlichens@gmail.com>
2017-11-16 10:12:10 -0800

    removeImage test now passing in actions test

--- src/__tests__/actions.test.js
+++ src/__tests__/actions.test.js
@@ -14,3 +14,29 @@ it('adds a new image', () => {
     bunnies: [ { _id: '12345', title: 'bunny', description: 'cute bunny', url: 'cutebunny.com' } ]
   });
 });
+
+it('removes an image', () => {
+  const testImageOne = {
+    _id: '12345',
+    title: 'bunny',
+    description: 'cute bunny',
+    url: 'cutebunny.com'
+  };
+  const testImageTwo = {
+    _id: '67890',
+    title: 'bunnies',
+    description: 'cutest bunny',
+    url: 'cutebunny.cute'
+  };
+
+  let state = { bunnies: [] };
+  state = addImage(state, testImageOne);
+  state = addImage(state, testImageTwo);
+
+  const id = state.bunnies[0]._id;
+  const newState = removeImage(state, id);
+
+  expect(newState).toEqual({
+    bunnies: [state.bunnies[1]]
+  });
+});
