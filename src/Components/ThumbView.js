ae16be2e1423d3a1f56e7c6b4cc459c0d55dab77
ckrountree <jmail0207@gmail.com>
2017-11-16 09:43:36 -0800

    create ThumbView

---
+++ src/Components/ThumbView.js
@@ -0,0 +1,22 @@
+import React, { PureComponent } from 'react';
+
+export default class ThumbView extends PureComponent {
+  render() {
+    const { bunnies } = this.props;
+
+    return (
+      <div>
+        {bunnies.map((bunny, i) => {
+          return (
+            <div>
+              <li key={i}>
+                <figcaption><a href={bunny.url}>{bunny.title}:</a></figcaption>
+                <img src={bunny.url} alt="" height="25%" width="25%"/>
+              </li>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
