60734f3596f55a43068aebd14fe38472fbd3380c
Zac-L <Zachlichens@gmail.com>
2017-11-16 09:47:10 -0800

    created action functions for addImage and remove

---
+++ src/data/actions.js
@@ -0,0 +1,26 @@
+
+
+export function addImage(state, newImage) {
+  return {
+    ...state,
+    bunnies: [
+      ...state.bunnies,
+      newImage
+    ]
+  };
+}
+
+
+export function removeImage(state, deleteImage) {
+  const index = state.bunnies.findIndex(image => image._id === deleteImage);
+
+  if(index === -1) return;
+
+  const images = state.bunnies.slice();
+  images.splice(index, 1);
+
+  return {
+    ...state,
+    bunnies: images
+  };
+}
