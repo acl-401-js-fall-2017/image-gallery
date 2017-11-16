8cfdb4122baa2b716484171b75ce9e701aefac8d
ckrountree <jmail0207@gmail.com>
2017-11-16 13:03:26 -0800

    start work on Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link, Redirect
+} from 'react-router-dom';
 import bunny from './image/bunny.jpg';
 import './App.css';
 import View from './View';
+// import ListView from './Components/ListView';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={bunny} className="App-logo" alt="" />
-          <h1 className="App-title">Welcome to a World of Bunnies</h1>
-        </header>
-        <View/>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={bunny} className="App-logo" alt="" />
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              {/* <Route exact path="/Components/ListView" component={ListView}/> */}
+              <Redirect to="/"/>
+            </Switch>
+          </header>
+          <View/>
+        </div>
+      </Router>
     );
   }
 }
+
+const Home = () => {
+  <div>
+    <h1 className="App-title">Welcome to a World of Bunnies</h1>
+    <Link to="/bunnies">View More Bunnies</Link>
+  </div>;
+};
+
+
 
