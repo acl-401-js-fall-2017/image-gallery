6f387dc38d2392364731cf1fb1ca0e32ab3149b4
Zac-L <Zachlichens@gmail.com>
2017-11-15 23:13:47 -0800

    added a view handler component to be passed in to App. View is working

--- src/components/View.js
+++ src/components/View.js
@@ -1,14 +1,16 @@
 import React, { PureComponent } from 'react';
-import Bunnies from './images/bunnies';
+
+import Bunnies from '../images/bunnies';
 import List from './List';
 import Thumbnail from './Thumbnail';
 import Gallery from './Gallery';
 
 export default class View extends PureComponent {
+
   constructor() {
     super();
     this.state = {
-      images: Bunnies,
+      bunnies: Bunnies,
       viewSelection: 'list'
     };
   }
@@ -21,12 +23,12 @@ export default class View extends PureComponent {
 
   render() {
 
-    const { images, viewSelection } = this.state;
+    const { viewSelection } = this.state;
     let displayView;
 
-    (viewSelection === 'list') && (displayView = <List images={images}/>)
-    (viewSelection === 'thumbnail') && (displayView = <Thumbnail images={images}/>)
-    (viewSelection === 'gallery') && (displayView = <Gallery images={images}/>)
+    (viewSelection === 'list') && (displayView = <List bunnies={this.state.bunnies}/>);
+    (viewSelection === 'thumbnail') && (displayView = <Thumbnail bunnies={this.state.bunnies}/>);
+    (viewSelection === 'gallery') && (displayView = <Gallery bunnies={this.state.bunnies}/>);
       
 
     return (
