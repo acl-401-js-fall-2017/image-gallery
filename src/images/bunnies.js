e6402e3b925793fa678caf133bb264cbc6349fbe
Zac-L <Zachlichens@gmail.com>
2017-11-16 14:48:24 -0800

    changed out shortid for now, snapshot testing was not working with shortid atm

--- src/images/bunnies.js
+++ src/images/bunnies.js
@@ -2,19 +2,19 @@ import shortid from 'shortid';
 
 const Bunnies = [
   {
-    _id: shortid.generate(),
+    _id: '1',
     title: 'Best Bunny',
     description: 'Just the cutest color on a bunny ever',
     url: 'https://s3.amazonaws.com/f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg'
   },
   {
-    _id: shortid.generate(),
+    _id: '2',
     title: 'Kind of cute Bunny',
     description: 'Too many carbs :(',
     url: 'http://static.boredpanda.com/blog/wp-content/uploads/2015/09/cute-bunnies-25__605.jpg'
   },
   {
-    _id: shortid.generate(),
+    _id: '3',
     title: 'Tiny Bunny',
     description: 'Cant see me',
     url: 'http://static.boredpanda.com/blog/wp-content/uploads/2015/09/cute-bunnies-110__605.jpg'
