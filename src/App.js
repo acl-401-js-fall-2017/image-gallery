f7f9d8c7a8748503321e264d803bdce8ecb4b200
Zac-L <Zachlichens@gmail.com>
2017-11-15 23:10:45 -0800

    added in view component, commented out the rest of the components for now. Want to make sure view is 100% working before I delete

--- src/App.js
+++ src/App.js
@@ -1,45 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Bunnies from './images/bunnies';
-import List from './components/List';
-import Thumbnail from './components/Thumbnail';
-import Gallery from './components/Gallery';
+// import Bunnies from './images/bunnies';
+// import List from './components/List';
+// import Thumbnail from './components/Thumbnail';
+// import Gallery from './components/Gallery';
+import View from './components/View';
+
 
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      view: 'List',
-      bunnies: Bunnies,
-      current: 0
-    };
-  }
-
-  handleClick(name, value) {
-    this.setState({
-      name: value
-    });
-  }
-
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     view: 'List',
+  //     bunnies: Bunnies,
+  //     current: 0
+  //   };
+  // }
 
   render() {
     return (
+      // <div className="App">
+      //   <div className="">
+      //     <List bunnies={this.state.bunnies} />
+      //   </div>
+      //   <div className="">
+      //     <Thumbnail bunnies={this.state.bunnies} />
+      //   </div>
+      //   <div className="">
+      //     <Gallery 
+      //       bunnies={this.state.bunnies} 
+      //       current={this.state.current} 
+      //       nextImage={this.nextImage}
+      //     />
+      //   </div>
+      // </div>
       <div className="App">
-        <div className="">
-          <List bunnies={this.state.bunnies} />
-        </div>
-        <div className="">
-          <Thumbnail bunnies={this.state.bunnies} />
-        </div>
-        <div className="">
-          <Gallery 
-            bunnies={this.state.bunnies} 
-            current={this.state.current} 
-            nextImage={this.nextImage}
-          />
-        </div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <View/>
       </div>
     );
   }
