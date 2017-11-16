bc513f63a742b1dd69e4f6a0c400e0b889d7f1ea
Zac-L <Zachlichens@gmail.com>
2017-11-16 12:10:32 -0800

    created component to handle adding an image using a form

---
+++ src/components/AddImage.js
@@ -0,0 +1,32 @@
+import React, { PureComponent } from  'react';
+import shortid from 'shortid';
+
+export default class AddImage extends PureComponent {
+
+  render() {
+
+    const { addImage } = this.props;
+
+    return (
+      <div>
+        <form onSubmit={event => {
+          event.preventDefault();
+          const { elements } = event.target;
+          const newImage = {
+            _id: shortid.generate(),
+            title: elements.title.value,
+            description: elements.description.value,
+            url: elements.url.value
+          };
+          elements.title.value = elements.description.value = elements.url.value = '';
+          return addImage(newImage);
+        }}>
+          <input name="title" placeholder="Title" />
+          <input name="description" placeholder="Description" />
+          <input name="url" placeholder="Url" />
+          <button type="submit">Add Image </button>
+        </form>
+      </div>
+    );
+  }
+}
