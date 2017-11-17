0890925bfeadd1d3709f4725cdec2f4367367687
mirrorapps <eli.speigel@gmail.com>
2017-11-16 21:15:54 -0800

    consolidated and added shorit

--- src/images.js
+++ src/images.js
@@ -1,21 +1,26 @@
-const image1 = { 
-  title: 'Cute Bunny',
-  description: 'Isn\'t it fuzzy-wuzzy cutest thing you\'ve ever seen?',
-  url: 'http://f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg'
-};
+import shortid from 'shortid';
 
-const image2 = { 
-  title: 'Cute Bunn',
-  description: 'Isn\'t it fuzzy-wuzzy cutest thing you\'ve ever seen?',
-  url: 'http://static.boredpanda.com/blog/wp-content/uploads/2015/09/cute-bunnies-25__605.jpg'
-};
+const images = [
+  {
+    _id: shortid.generate(), 
+    title: 'Cute Bunny',
+    description: 'Isn\'t it fuzzy-wuzzy cutest thing you\'ve ever seen?',
+    url: 'http://f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg'
+  },
 
-const image3 = { 
-  title: 'Cute Bun',
-  description: 'Isn\'t it fuzzy-wuzzy cutest thing you\'ve ever seen?',
-  url: 'http://static.boredpanda.com/blog/wp-content/uploads/2015/09/cute-bunnies-110__605.jpg'
-};
+  {
+    _id: shortid.generate(),  
+    title: 'Cute Bunn',
+    description: 'Isn\'t it fuzzy-wuzzy cutest thing you\'ve ever seen?',
+    url: 'http://static.boredpanda.com/blog/wp-content/uploads/2015/09/cute-bunnies-25__605.jpg'
+  },
+  { 
+    _id: shortid.generate(), 
+    title: 'Cute Bun',
+    description: 'Isn\'t it fuzzy-wuzzy cutest thing you\'ve ever seen?',
+    url: 'http://static.boredpanda.com/blog/wp-content/uploads/2015/09/cute-bunnies-110__605.jpg'
+  }
+];
 
-const images = [image1, image2, image3];
 
 export default images;
