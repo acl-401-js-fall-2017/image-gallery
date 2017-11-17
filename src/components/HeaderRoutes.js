f41036a4889712be432a41263f9a87a5c460ccfa
Zac-L <Zachlichens@gmail.com>
2017-11-17 14:59:35 -0800

    created a header component to handle some routes as navLinks in the header itself

---
+++ src/components/HeaderRoutes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import logo from '../logo.svg';
+
+const HeaderRoutes = props => <NavLink {...props} 
+  className="nav-link"
+  activeClassName="active"
+/>;
+
+export default function Header() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+      <nav>
+        <li>
+          <HeaderRoutes exact to='/list'>List</HeaderRoutes>
+        </li>
+        <li>
+          <HeaderRoutes exact to='/thumbnail'>Thumbnail</HeaderRoutes>
+        </li>
+        <li>
+          <HeaderRoutes exact to='/gallery'>Gallery</HeaderRoutes>
+        </li>
+      </nav>
+    </header>
+  );
+}
