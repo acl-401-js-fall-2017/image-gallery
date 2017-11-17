d325531c4c495c95a5fccdee1b74a3adc6d455d7
Zac-L <Zachlichens@gmail.com>
2017-11-17 14:04:26 -0800

    moved header code to a seperate component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './App.css';
 
 import View from './components/View';
+import HeaderRoutes from './components/HeaderRoutes';
 
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <View/>
-      </div>
+      <Router>
+        <div className="App">
+          <HeaderRoutes/>
+          <View/>
+        </div>
+      </Router>
     );
   }
 }
