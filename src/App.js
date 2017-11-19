56d55d27a2539ba215dc4928ef424a3d72597066
ckrountree <jmail0207@gmail.com>
2017-11-19 14:49:52 -0800

    create Header component to handle routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch, Route, Link, Redirect
-} from 'react-router-dom';
-import flops from './image/flops.png';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header';
 import View from './View';
-import About from './Components/About';
+// import About from './Components/About';
 
 export default class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <header className="App-header">
-            <img src={flops} className="App-logo" alt="" />
-            <Switch>
-              <Route exact path="/" component={Home}/>
-              <Route path="/about" component={About}/>
-              <Route exact path="/view" component={View}/>
-              <Redirect to="/"/>
-            </Switch>
-          </header>
+          <Header/>
+          <View/>
         </div>
       </Router>
     );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,22 +1,31 @@
-// import React from 'react';
-// import { NavLink } from 'react-router-dom';
-// import bunny from './image/bunny.jpg';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import flops from '../image/flops.png';
 
-// const HeaderLink = props => <NavLink {...props} 
-//   className="nav-link" 
-//   activeClassName="active"
-// />;
+const HeaderRoutes = props => <NavLink {...props} 
+  className="nav-link"
+  activeClassName="active"
+/>;
 
-// export default function Header() {
-//   return (
-//     <header className="App-header">
-//       <img src={bunny} className="App-logo" alt="" />
-//       <h1 className="App-title">Welcome to a World of Bunnies</h1>
-//       <nav>
-//         <li>
-//           <HeaderLink exact to="/"> Home</HeaderLink>
-//         </li>
-//       </nav>
-//     </header>
-//   );
-// }
+export default function Header() {
+  return (
+    <header className="App-header">
+      <img src={flops} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to a World of Bunnies</h1>
+      <nav>
+        <li>
+          <HeaderRoutes to='/'>Home</HeaderRoutes>
+        </li>
+        <li>
+          <HeaderRoutes to="/list">List</HeaderRoutes>
+        </li>
+        <li>
+          <HeaderRoutes to="/thumbnail">Thumbnail</HeaderRoutes>
+        </li>
+        <li>
+          <HeaderRoutes to="/gallery">Gallery</HeaderRoutes>
+        </li>
+      </nav>
+    </header>
+  );
+}
