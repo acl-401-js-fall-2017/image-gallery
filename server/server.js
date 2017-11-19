64355c5344a68b955ffff430a03c6f6c5430c27f
Zac-L <Zachlichens@gmail.com>
2017-11-19 15:12:21 -0800

    removed server folder to its open dir/repo

--- server/server.js
+++
@@ -1,8 +0,0 @@
-require('./lib/setup-mongoose');
-const app = require('./lib/app');
-const http = require('http');
-const port = process.env.PORT || 3001;
-
-const server = http.createServer(app);
-
-server.listen('server running at PORT: ', server.address());
