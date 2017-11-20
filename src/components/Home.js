4caa43da24730aa65ac5e4a3ece101a9a532c9c7
Zac-L <Zachlichens@gmail.com>
2017-11-20 09:36:51 -0800

    built out a quick home page component

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+
+export default class Home extends PureComponent {
+    
+  render() {
+
+    return (
+      <div>
+        <h1>Welcome to the Image Gallery Home page!</h1>
+        {/* <Link to="/">View Images</Link> */}
+      </div>
+    );
+  }
+}
