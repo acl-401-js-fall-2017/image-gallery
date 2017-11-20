d377a358097b16331e5fe16f257be4a8150621aa
mirrorapps <eli.speigel@gmail.com>
2017-11-19 18:46:19 -0800

    nsig

--- src/images.js
+++ src/images.js
@@ -7,7 +7,6 @@ const images = [
     description: 'Isn\'t it fuzzy-wuzzy cutest thing you\'ve ever seen?',
     url: 'http://f.cl.ly/items/3g3J1G0w122M360w380O/3726490195_f7cc75d377_o.jpg'
   },
-
   {
     _id: shortid.generate(),  
     title: 'Cute Bunn',
@@ -21,6 +20,5 @@ const images = [
     url: 'http://static.boredpanda.com/blog/wp-content/uploads/2015/09/cute-bunnies-110__605.jpg'
   }
 ];
-
 
 export default images;
