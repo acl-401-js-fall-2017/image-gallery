dbe21e20509bfa26af934ed343082d7076662ae4
Zac-L <Zachlichens@gmail.com>
2017-11-20 11:32:05 -0800

    created albums component to grab all avaliable albums

---
+++ src/components/Albums.js
@@ -0,0 +1,47 @@
+import React, { PureComponent } from 'react';
+import { Link } from 'react-router-dom';
+import albumApi from '../service/album.api';
+import { loadAlbums, addImage, removeImage } from '../data/actions';
+import AddImage from '../components/AddImage';
+
+export default class Albums extends PureComponent {
+    state = {
+      albums: []
+    }
+
+    async componentDidMount() {
+      const albums = await albumApi.get();
+      const newState = loadAlbums(this.state, albums);
+      this.setState(newState);
+    }
+
+    handleAdd = async ({ title }) => {
+      const album = await albumApi.add({ title });
+      const newState = addImage(this.state, album);
+      this.setState(newState);
+    }
+
+    handleRemove = async id => {
+      await albumApi.remove(id);
+      const newState = removeImage(this.state, id);
+      this.setState(newState);
+    }
+
+    render() {
+      const { albums } = this.state;
+
+      return(
+        <section>
+          <h1>Hello!</h1>
+          <ul className="items">
+            {albums.map(album => (
+              <li key={album._id}>
+                <Link to={`/albums/${album._id}`}>{album.name}</Link>
+                {/* <button onClick={() => this.handleRemove(album._id)}>X</button> */}
+              </li>
+            ))}
+          </ul>
+        </section>
+      );
+    }
+}
