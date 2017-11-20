04b3bd207a9530d0e45ac6e2a2177cd30941f38a
Shane Moyo <smoyo@pdx.edu>
2017-11-20 05:29:45 -0800

    added short id

--- src/components/actions.js
+++ src/components/actions.js
@@ -1,5 +1,9 @@
+import shortid from 'shortid';
+
+
 export function onDelete(imageId, state) {
-  const position = state.images.findIndex(img => img._id === imageId);
+  const position = state.images.findIndex(img => img._id === parseInt(imageId, 10));
+  if (position === -1) return state;
   state.images.splice(position, 1);
   const images = state.images.slice();
   return {
@@ -9,6 +13,7 @@ export function onDelete(imageId, state) {
 }
 
 export function onAdd(imageData, state) {
+  imageData._id = shortid.generate();
   const images = state.images.slice();
   images.push(imageData);
   return {
