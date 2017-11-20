9785b434b9bc6b25055ccd7cdb7668e28e5cfa6a
Shane Moyo <smoyo@pdx.edu>
2017-11-20 06:34:00 -0800

    added actions test

--- src/components/actions.js
+++ src/components/actions.js
@@ -1,7 +1,11 @@
+import shortid from 'shortid';
+
+
 export function onDelete(imageId, state) {
-  const position = state.images.findIndex(img => img._id === imageId);
-  state.images.splice(position, 1);
+  const position = state.images.findIndex(img => img._id === parseInt(imageId, 10));
+  if (position === -1) return state;
   const images = state.images.slice();
+  images.splice(position, 1);
   return {
     ...state,
     images
@@ -9,6 +13,7 @@ export function onDelete(imageId, state) {
 }
 
 export function onAdd(imageData, state) {
+  imageData._id = shortid.generate();
   const images = state.images.slice();
   images.push(imageData);
   return {
