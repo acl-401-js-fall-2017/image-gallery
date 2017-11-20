0cee7808b193b751daaeffbf028a9af40bbd6051
Zac-L <Zachlichens@gmail.com>
2017-11-20 11:31:47 -0800

    created an Album component to grab 1 album

---
+++ src/components/Album.js
@@ -0,0 +1,50 @@
+import React, { PureComponent } from 'react';
+import { Link } from 'react-router-dom';
+import albumApi from '../service/album.api';
+import { loadAlbums, addImage, removeImage } from '../data/actions';
+import AddImage from '../components/AddImage';
+
+export default class Album extends PureComponent {
+    state = {
+      images: []
+    }
+
+    async componentDidMount() {
+      const albums = await albumApi.getById(this.props.match.params.id);
+      const newState = loadAlbums(this.state, albums);
+      this.setState(newState);
+    }
+
+    handleAdd = async ({ title }) => {
+      const album = await albumApi.add({ title });
+      const newState = addImage(this.state, album);
+      this.setState(newState);
+    }
+
+    handleRemove = async id => {
+      await albumApi.remove(id);
+      const newState = removeImage(this.state, id);
+      this.setState(newState);
+    }
+
+    render() {
+      const { images } = this.state;
+
+      return(
+        <section>
+          <h3> ALLLLBUUUUMMMMMSSSSS!!!!!!!!</h3>
+          <ul className="items">
+            {images.map(image => (
+              <li key={image._id}>
+                <Link to={`/albums/${image._id}`}>{image.title}</Link>
+                <button onClick={() => this.handleRemove(image._id)}>X</button>
+              </li>
+            ))}
+          </ul>
+          <AddImage type="list" onAdd={this.handleAdd}>
+            <input name="title"/>
+          </AddImage>
+        </section>
+      );
+    }
+}
