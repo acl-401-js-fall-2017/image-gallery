0047d0a416e237d6a7c9e25ed3de1bfc1137684b
mirrorapps <eli.speigel@gmail.com>
2017-11-21 14:24:20 -0800

    add and remove functions

---
+++ src/Actions/image.actions.js
@@ -0,0 +1,21 @@
+export function addImage(state, newImage) {
+  const newState = [
+    ...state,
+    newImage
+  ];
+  return newState;
+}
+
+export function removeImage(state, targetImage) {
+  const index = state.images.findIndex(image => image._id === targetImage);
+  if (index === -1) return;
+
+  const images = state.images.slice();
+  images.splice(index, 1);
+
+  return {
+    ...state,
+    
+  };
+
+}
