48d58da6dd25ed598ed7492ff36bd5d2c477a486
mirrorapps <eli.speigel@gmail.com>
2017-11-22 18:27:52 -0800

    moving images to selector

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
 import React, { Component } from 'react';
 import Selector from './Components/Selector';
-import images from './images';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      images
-    };
-    
-  }
-
-  handleAdd = images => {
-    this.setState({ images });
-  }
-
-  handleRemove = images => {
-    this.setState({ images });
-  }
   render() {
 
     return (
@@ -26,7 +10,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Selector images={images} onAdd={this.handleAdd} onRemove={this.handleRemove}/>
+        <Selector />
       </div>
     );
   }
