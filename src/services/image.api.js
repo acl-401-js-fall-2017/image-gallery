67743ed06c6bff4548d6f85ae441bddf53e20586
ckrountree <jmail0207@gmail.com>
2017-11-25 15:03:19 -0800

    add album and image apis

---
+++ src/services/image.api.js
@@ -0,0 +1,15 @@
+import api from './api';
+
+export default {
+  get() {
+    return api.get('/images');
+  },
+
+  add(images) {
+    return api.post('/images', images);
+  },
+
+  remove(id) {
+    return api.fetch(`/images/${id}`);
+  }
+};
