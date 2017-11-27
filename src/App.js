23dc7a6e91139a8d0e372bb8770785a018043d12
mirrorapps <eli.speigel@gmail.com>
2017-11-26 22:45:01 -0800

    added routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
-import Selector from './Components/Selector';
+import { 
+  BrowserRouter as Router, 
+  Route, Switch, Redirect, 
+  Link  } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header';
+import View from './Components/View';
 
 class App extends Component {
   render() {
-
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Selector />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/images" component={View}/>
+            <Route exact path="/about" component={About}/>
+            <Redirect to="/"/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
+
+const Home = () => (
+  <div>
+    <h1>Welcome to the Image Gallery!</h1>
+    <Link to="/images">View the Images</Link>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h1>About the Image Gallery</h1>
+    <p>This is an image gallery made with react.</p>
+  </div>
+);
 
 export default App;
