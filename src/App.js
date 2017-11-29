530e5b56776cd781e95e12b5218861cb75ca89c1
Shane Moyo <smoyo@pdx.edu>
2017-11-29 05:23:10 -0800

    reset app.js for travis

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+import { 
+  BrowserRouter as Router, 
+  Route, Switch, Redirect } from 'react-router-dom';
+import Albums from './components/Albums';
+import View from './components/View';
+import Header from './components/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Header/>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/albums" component={Albums}/>
+            <Route exact path="/albums/:id" component={View}/>
+            <Route path="/about" component={About}/>
+            <Redirect to="/"/>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+const Home = () => (
+  <div>
+    <h1>Welcome to my Image Gallery!</h1>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h1>Welcome to my about Page!</h1>
+  </div>
+);
+
+
+export default App;
