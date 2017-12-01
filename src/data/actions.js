70f3cb219e4dafb29e22220e841f9728db08bcd3
Zac-L <Zachlichens@gmail.com>
2017-11-30 20:13:12 -0800

    added addAlbum function

--- src/data/actions.js
+++ src/data/actions.js
@@ -10,24 +10,34 @@ export function loadAlbums(state, albums) {
 export function addImage(state, newImage) {
   return {
     ...state,
-    bunnies: [
-      ...state.bunnies,
+    images: [
+      ...state.images,
       newImage
+    ]
+  };
+}
+
+export function addAlbum(state, newAlbum) {
+  return {
+    ...state,
+    albums: [
+      ...state.albums,
+      newAlbum
     ]
   };
 }
 
 
 export function removeImage(state, deleteImage) {
-  const index = state.bunnies.findIndex(image => image._id === deleteImage);
+  const index = state.images.findIndex(image => image._id === deleteImage);
 
   if(index === -1) return;
 
-  const images = state.bunnies.slice();
+  const images = state.images.slice();
   images.splice(index, 1);
 
   return {
     ...state,
-    bunnies: images
+    images: images
   };
 }
