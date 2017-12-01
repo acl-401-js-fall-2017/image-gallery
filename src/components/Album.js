a332b3f93bf13cffe5aa8c223117f89668b729ce
Zac-L <Zachlichens@gmail.com>
2017-11-30 20:18:17 -0800

    moved some routes from view to album, fixed handleAdd / remove function. Created function to handle getting AlbumId

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,9 +1,11 @@
 import React, { PureComponent } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 import albumApi from '../service/album.api';
 import imagesApi from '../service/images.api';
-import { loadAlbums, addImage, removeImage } from '../data/actions';
-import AddImage from '../components/AddImage';
+import { addImage, removeImage } from '../data/actions';
+import List from './List';
+import Thumbnail from './Thumbnail';
+import Gallery from './Gallery';
 
 export default class Album extends PureComponent {
     state = {
@@ -15,34 +17,59 @@ export default class Album extends PureComponent {
       this.setState(album);
     }
 
-    handleAdd = async ({ title }) => {
-      const album = await albumApi.add({ title });
-      const newState = addImage(this.state, album);
+    handleAdd = async newImage => {
+      newImage.album = this.props.match.params.id;
+      const image = await imagesApi.add(newImage);
+      const newState = addImage(this.state, image);
       this.setState(newState);
     }
 
     handleRemove = async id => {
-      await albumApi.remove(id);
+      await imagesApi.remove(id);
       const newState = removeImage(this.state, id);
       this.setState(newState);
     }
 
-    render() {
-      const { images } = this.state;
-      const { addImage } = this.props;
+    getAlbumId () {
+      return this.props.match.params.id;
+    }
 
+    render() {
+      // const { handleRemove } = this.props;
+
+      const displayView = {
+        
+        list: () => <List images={this.state.images}
+          handleRemove={imageId => {
+            return this.handleRemove(imageId);
+          }}
+          addImage={image => this.handleAdd(image)}
+        />,
+        
+        thumbnail: () => <Thumbnail images={this.state.images}/>,
+        
+        gallery: () => <Gallery images={this.state.images}/>
+        
+      };
+      const HeaderRoutes = props => <NavLink {...props}/>;
       return(
         <section>
+          <Route exact path={`/albums/${this.getAlbumId()}/gallery`} component={displayView.gallery}/>
+          <Route exact path={`/albums/${this.getAlbumId()}/list`} component={displayView.list}/>
+          <Route exact path={`/albums/${this.getAlbumId()}/thumbnail`} component={displayView.thumbnail}/>
+          <div className="image-links">
+            <li>
+              <HeaderRoutes exact to={`/albums/${this.getAlbumId()}/list`}>List</HeaderRoutes>
+            </li>
+            <li>
+              <HeaderRoutes exact to={`/albums/${this.getAlbumId()}/thumbnail`}>Thumbnail</HeaderRoutes>
+            </li>
+            <li>
+              <HeaderRoutes exact to={`/albums/${this.getAlbumId()}/gallery`}>Gallery</HeaderRoutes>
+            </li>
+          </div>
+
           <h3> ALLLLBUUUUMMMMMSSSSS!!!!!!!!</h3>
-          <ul className="items">
-            {images.map(image => (
-              <li key={image._id}>
-                <Link to={`/albums/${image._id}`}>{image.title}</Link>
-                <button onClick={() => this.handleRemove(image._id)}>x</button>
-              </li>
-            ))}
-          </ul>
-          <AddImage type="list" addImage={newImage => addImage(newImage)}/>
         </section>
       );
     }
